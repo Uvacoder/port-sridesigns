fb244d545acb1c46411cb8caad776339cdafcc5c
Sriram Venugopal <48489373+sridesigns@users.noreply.github.com>
2021-08-22 17:19:56 +0530

    🐛  Included header component to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 
 const Home: NextPage = () => {
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
       <h1>Welcome to my portfolio!</h1>
     </div>
   )
