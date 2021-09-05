e57aa03b8d3ffaf96dd90911ce49817ebdda02f1
Sriram Venugopal <48489373+sridesigns@users.noreply.github.com>
2021-09-06 00:36:54 +0530

    🐛 Asset folder added to tailwind purge 4 SVG fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 const { fontWeight } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/assets/**/*.{js,ts,jsx,tsx}',],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
